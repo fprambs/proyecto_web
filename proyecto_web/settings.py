b2e2247877182b01ec4c4b0f939d4570c2ed8b92
Felipe Prambs <felipe._16@live.com>
2016-12-20 15:03:00 -0300

    tercer commit

--- proyecto_web/settings.py
+++ proyecto_web/settings.py
@@ -78,12 +78,14 @@ WSGI_APPLICATION = 'proyecto_web.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'arrendados',
-        'USER': 'root',
-        'PASSWORD': '1029',
-        'HOST': 'localhost',
-        'PORT': '3306 ',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'ENGINE': 'django.db.backends.mysql',
+        #'NAME': 'arrendados',
+        #'USER': 'root',
+        #'PASSWORD': '1029',
+        #'HOST': 'localhost',
+        #'PORT': '3306 ',
     }
 }
 
