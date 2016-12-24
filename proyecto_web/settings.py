7595aa5eaf7d5316d8fa4835b97fd36ae79cea88
Felipe Prambs <felipe._16@live.com>
2016-12-24 23:32:59 +0000

    Primir commit a la rama

--- proyecto_web/settings.py
+++ proyecto_web/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'arrendados',
-    'rest_framework',
 
 ]
 
@@ -78,11 +77,13 @@ WSGI_APPLICATION = 'proyecto_web.wsgi.application'
 
 DATABASES = {
     'default': {
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'arrendados',
         'USER': 'root',
-        'PASSWORD': '1029',
-        'HOST': 'localhost',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
         'PORT': '3306 ',
     }
 }
